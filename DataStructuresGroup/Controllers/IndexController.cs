8a503f3953c38ead76493b6687fd644cf0746fa5
nmmahe <nicholasmahe@gmail.com>
2018-10-09 15:47:12 -0600

    uploading to git

---
+++ DataStructuresGroup/Controllers/IndexController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructuresGroup.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Stack()
+        {
+            return View();
+        }
+
+        public ActionResult Queue()
+        {
+            return View();
+        }
+
+        public ActionResult Dictionary()
+        {
+            return View();
+        }
+
+        public ActionResult Exit()
+        {
+            return View();
+        }
+    }
+}
