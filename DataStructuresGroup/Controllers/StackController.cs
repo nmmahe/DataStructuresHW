2775e8c41936dc15ae54a61f42082cd0b1afbbd5
nmmahe <nicholasmahe@gmail.com>
2018-10-15 09:21:53 -0600

    branch with stack

--- DataStructuresGroup/Controllers/StackController.cs
+++ DataStructuresGroup/Controllers/StackController.cs
@@ -8,13 +8,17 @@ namespace DataStructuresGroup.Controllers
 {
     public class StackController : Controller
     {
+        public 
 
         static Stack<string> myStack = new Stack<string>();
+        
         // GET: Stack
         public ActionResult Index()
         {
 
             ViewBag.Stack = myStack;
+            ViewBag.Hide = "StackHide";
+            //ViewBag.Search = search;
             return View();
         }
 
@@ -26,8 +30,97 @@ namespace DataStructuresGroup.Controllers
             "New Entry " + (myStack.Count + 1)
             );
 
+            //ViewBag.Stack=myStack;
+            return View("Index");
+        }
+
+        public ActionResult HugeList()
+        {
+            myStack.Clear();
+            for (int iCount=0; iCount<2000; iCount++)
+            {
+                myStack.Push(
+                    "New Entry " + (myStack.Count + 1)
+                    );
+                //ViewBag.Stack = myStack;
+
+            }
+            return View("Index");
+        }
+
+        public ActionResult DisplayStack()
+        {
+            ViewBag.Hide = "StackShow";
+            ViewBag.Stack = myStack;
+            return View("Index"); 
+            
+        }
+
+        public ActionResult DeleteStack()
+        {
+            myStack.Pop();
+
             ViewBag.Stack = myStack;
             return View("Index");
+
+        }
+
+        public ActionResult ClearStack()
+        {
+            myStack.Clear();
+
+            ViewBag.Stack = myStack;
+            return View("Index");
+
+        }
+
+        public ActionResult SearchStack()
+        {
+            //make input tag
+            //var search = "<input type='text' id='mySearch' onkeyup='myFunction()' placeholder='Search..' title='Type in an entry'>";
+            //
+
+            /*foreach (string check in myStack)
+            {
+                if (check == "New Entry 5")
+                {
+                    return 
+                }
+            }*/
+            ViewBag.Stack = myStack;
+            foreach(var i in myStack)
+            {
+                if (i=="New Entry 7")
+                {
+                    ViewBag.Search = "New Entry 7 is found!";
+                    break;
+                }
+                else
+                {
+                    ViewBag.Search = "New Entry 7 is not found!";
+                }
+            }
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            //loop to do all the work
+
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+
+            ViewBag.StopWatch = ts;
+
+
+
+            return View("Index");
+        }
+
+        public ActionResult MainMenu()
+        {
+            return RedirectToAction("Index","Index");
         }
     }
 }
