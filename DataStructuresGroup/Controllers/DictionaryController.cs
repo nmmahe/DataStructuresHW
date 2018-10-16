92b4962147871d42c5a1aed1d6a8b53235e4a852
cnelson27 <cnelson27@outlook.com>
2018-10-15 20:29:37 -0600

    my start on a dictionary

--- DataStructuresGroup/Controllers/DictionaryController.cs
+++ DataStructuresGroup/Controllers/DictionaryController.cs
@@ -8,11 +8,118 @@ namespace DataStructuresGroup.Controllers
 {
     public class DictionaryController : Controller
     {
+        static Dictionary<string, int> myDictionary = new Dictionary<string, int>();
         // GET: Dictionary
         public ActionResult Index()
         {
-            //ViewBag.Dictionary = myDictionary;
-            return View();
+            ViewBag.Dictionary = myDictionary;
+            ViewBag.Hide = "StackHide";
+            return View("Index");
+        }
+
+        public ActionResult AddOne()
+        {
+            myDictionary.Add(
+            "New Entry " + (myDictionary.Count + 1), myDictionary.Count + 1
+            );
+
+            return View("Index");
+        }
+
+        public ActionResult HugeList()
+        {
+            myDictionary.Clear();
+            for (int iCount = 0; iCount < 2000; iCount++)
+            {
+                myDictionary.Add(
+            "New Entry " + (myDictionary.Count + 1), myDictionary.Count + 1
+            );
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult DisplayDictionary()
+        {
+            if (myDictionary.Count == 0)
+            {
+                ViewBag.errormsg = "No entries in dictionary";
+            }
+            else
+            {
+                ViewBag.Hide = "StackShow";
+                ViewBag.Dictionary = myDictionary.Keys;
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult DeleteDictionary()
+        {
+            if (myDictionary.Count > 0)
+            {
+                myDictionary.Remove("New Entry " + (myDictionary.Count));
+            }
+            else
+            {
+                ViewBag.errormsg = "No entries in dictionary";
+            }
+
+            ViewBag.Dictionary = myDictionary;
+            return View("Index");
+
+        }
+
+        public ActionResult ClearDictionary()
+        {
+            myDictionary.Clear();
+
+            ViewBag.Dictionary = myDictionary;
+            return View("Index");
+
+        }
+
+        public ActionResult SearchDictionary()
+        {
+            if (myDictionary.Count != 0)
+            {
+                ViewBag.Dictionary = myDictionary;
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+                sw.Start();
+
+                foreach (var i in myDictionary.Keys)
+                {
+                    if (i == "New Entry 7")
+                    {
+                        ViewBag.Search = "New Entry 7 is found!";
+                        break;
+                    }
+                    else
+                    {
+                        ViewBag.Search = "New Entry 7 is not found!";
+                    }
+                }
+
+                sw.Stop();
+
+                TimeSpan ts = sw.Elapsed;
+
+                ViewBag.StopWatch = ts;
+            }
+            else
+            {
+                ViewBag.errormsg = "No entries found";
+            }
+
+
+            return View("Index");
+
+        }
+
+        public ActionResult MainMenu()
+        {
+            return RedirectToAction("Index", "Index");
         }
     }
 }
